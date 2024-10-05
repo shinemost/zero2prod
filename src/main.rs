2e863e769a807a59729e803bcebcf6056f9efe8f
shinemost <supertain147@163.com>
2024-10-05 17:36:46 +0800

    增加health_check端点

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+async fn health_check(_req: HttpRequest) -> impl Responder {
+   HttpResponse::Ok()
 }
-
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| { App::new()
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
+        .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
         .run()
         .await
 }
+// curl -i http://127.0.0.1:8000/health_check
