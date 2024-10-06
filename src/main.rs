a80be2238dab4ff52436157a4c2aaeab0900c105
shinemost <supertain147@163.com>
2024-10-06 17:02:12 +0800

    项目结构改造，实现第一个集成测试

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-async fn health_check(_req: HttpRequest) -> impl Responder {
-   HttpResponse::Ok()
-}
+use zero2prod::run;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| { App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
-// curl -i http://127.0.0.1:8000/health_check
+// curl -v http://127.0.0.1:8000/health_check
