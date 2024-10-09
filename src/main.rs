4c57134f73042036d60d33916f54f775b1f5f7ec
shinemost <supertain147@163.com>
2024-10-09 23:01:39 +0800

    重构项目结构，将可观测性的代码提出到telemetry.rs中

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Unable to setup log tracer.");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let fmt_layer = BunyanFormattingLayer::new("zero2prod".into(), || std::io::stdout());
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(fmt_layer);
-    set_global_default(subscriber).expect("Setting default subscriber failed");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.get_connection_string())
         .await
