71c7f4e4fff1b9f10fb9e514eaa3e8755dd8517e
shinemost <supertain147@163.com>
2024-10-10 00:04:34 +0800

    集成测试中的日志进行相应配置

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,13 +2,23 @@ use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+// 高级特质约束
+// Sink会实现MakeWriter特质，无论生命周期参数a是什么
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let fmt_layer = BunyanFormattingLayer::new(name, || std::io::stdout());
+    let fmt_layer = BunyanFormattingLayer::new(name, sink);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,23 @@
 //! tests/health_check.rs
 
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -35,6 +49,10 @@ async fn health_check_works() {
 
 // Launch our application in the background ~somehow~
 async fn spawn_app() -> TestApp {
+    // 只会在第一次使用TRACING时会调用initialize
+    // 其他时候都会直接跳过
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
@@ -123,3 +141,5 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+// cargo install bunyan
+// TEST_LOG=true cargo test health_check_works | bunyan
