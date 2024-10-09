598600cde916813e996919b452c833f832565179
shinemost <supertain147@163.com>
2024-10-09 11:47:03 +0800

    完全迁移至tracing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // 如果环境变量RUST_LOG未被设置，则默认输出所有info及以上级别的日志
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let fmt_layer = BunyanFormattingLayer::new("zero2prod".into(), || std::io::stdout());
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(fmt_layer);
+    set_global_default(subscriber).expect("Setting default subscriber failed");
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.get_connection_string())
         .await
